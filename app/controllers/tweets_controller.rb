1c2620aa124be8cf59782be496338ba16631bb33
yoshimori <2311140023p@ed.fuk.kindai.ac.jp>
2025-10-18 08:52:24 +0000

    ツイート機能追加

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,21 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    tweet = Tweet.new(message: params[:tweet][:message],user_id: 2)
+    tweet.save
+    redirect_to tweets_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to tweets_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/new"
-  get "users/create"
-  get "users/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,5 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users
-  root 'users#index'
+  resources :tweets
+  root 'tweets#index'
 end
